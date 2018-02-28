8c2a8f62bce8481f984d90bb9ac276de62a8f090
Eric McKinney <mckinney99@gmail.com>
2018-02-28 22:26:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const num = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < num.length; i++ ) {
+    num[i].innerHTML = (i + n)
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+
+}
